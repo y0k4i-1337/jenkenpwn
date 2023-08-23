252e35898714082d23513eab4066ed44453d03e1
Mayk <7395296+y0k4i-1337@users.noreply.github.com>
2023-08-23 17:37:44 -0300

    Get jobs recursively

--- src/core/client.rs
+++ src/core/client.rs
@@ -44,7 +44,7 @@ impl JenkinsClient {
     }
 
     /// Perform a GET request to the given path and return the response as a string
-    pub async fn get(&self, path: &str) -> Result<String, Box<dyn std::error::Error>> {
+    pub async fn get_path(&self, path: &str) -> Result<String, Box<dyn std::error::Error>> {
         let url = concatenate_url(&self.url, path)?;
         debug!("GET {}", url);
         let mut request = self.client.get(&url);
@@ -61,6 +61,26 @@ impl JenkinsClient {
             Err(format!("Error: {}", response.status()).into())
         }
     }
+
+    /// Perform a GET request to the given url and return the response as a
+    /// string
+    pub async fn get_url(&self, url: &str) -> Result<String, Box<dyn std::error::Error>> {
+        debug!("GET {}", url);
+        let mut request = self.client.get(url);
+
+        if let Some(credentials) = &self.credentials {
+            request =
+                request.basic_auth(credentials.get_username(), Some(credentials.get_password()));
+        }
+
+        let response = request.send().await?;
+        if response.status().is_success() {
+            Ok(response.text().await?)
+        } else {
+            Err(format!("Error: {}", response.status()).into())
+        }
+    }
+
 }
 
 impl Credentials {
--- src/core/dump.rs
+++ src/core/dump.rs
@@ -1,6 +1,8 @@
 use super::client::JenkinsClient;
 use crate::logger::init_logger;
 use crate::utils::search_substring;
+use async_recursion::async_recursion;
+use log::{warn, debug};
 
 pub struct Dumper {
     pub client: JenkinsClient,
@@ -30,7 +32,7 @@ impl Dumper {
 
     /// Dump all jobs
     pub async fn dump_jobs(&self) -> Result<serde_json::Value, Box<dyn std::error::Error>> {
-        let response = self.client.get("/api/json/").await?;
+        let response = self.client.get_path("/api/json").await?;
         if search_substring(&response, r"Authentication required") {
             return Err("Authentication required".into());
         } else if search_substring(&response, r"Invalid password/token") {
@@ -39,8 +41,69 @@ impl Dumper {
             return Err("Missing the Overall/Read permission".into());
         } else {
             let response: serde_json::Value = serde_json::from_str(&response)?;
-            let jobs: serde_json::Value = response["jobs"].clone();
-            Ok(jobs)
+
+            // extract jobs from the parsed JSON and create a new JSON object
+            let mut jobs_array = Vec::new();
+            if let Some(jobs) = response.get("jobs").and_then(|jobs| jobs.as_array()) {
+                debug!("Found {} jobs", jobs.len());
+                debug!("Retrieving job info recursively");
+                for job in jobs {
+                    if let Some(job_url) = job.get("url").and_then(|url| url.as_str()) {
+                        match self.get_jobs_recursive(job_url).await {
+                            Ok(job_info) => jobs_array.push(job_info),
+                            Err(e) => warn!("Error: {}", e),
+                        }
+                    }
+                }
+            }
+            Ok(serde_json::Value::Array(jobs_array))
         }
+    }
+
+    /// Get job information recursively
+    #[async_recursion]
+    async fn get_jobs_recursive(
+        &self,
+        job_url: &str,
+    ) -> Result<serde_json::Value, Box<dyn std::error::Error>> {
+        // Make a GET request to retrieve job information
+        debug!("Retrieving job info from: {}", job_url);
+        let response = self
+            .client
+            .get_url(format!("{}/api/json", job_url).as_str())
+            .await?;
+
+        // Parse the JSON response
+        let json: serde_json::Value = serde_json::from_str(&response)?;
+
+        // Create a JSON object to store job information
+        let mut job_info = serde_json::json!({
+            "name": json.get("name").and_then(|n| n.as_str()),
+            "url": json.get("url").and_then(|url| url.as_str()),
+        });
+
+        // If the job has sub-jobs, recursively process them
+        if let Some(sub_jobs) = json.get("jobs").and_then(|jobs| jobs.as_array()) {
+            let mut sub_jobs_info = Vec::new();
+            for sub_job in sub_jobs {
+                if let Some(sub_job_url) = sub_job.get("url").and_then(|url| url.as_str()) {
+                    let sub_job_info = self.get_jobs_recursive(sub_job_url).await?;
+                    sub_jobs_info.push(sub_job_info);
+                }
+            }
+            job_info["sub_jobs"] = serde_json::Value::Array(sub_jobs_info);
+        }
+
+        // If the job has builds, include their URLs
+        if let Some(builds) = json.get("builds").and_then(|builds| builds.as_array()) {
+            let build_urls: Vec<serde_json::Value> = builds
+                .iter()
+                .filter_map(|build| build.get("url").and_then(|url| url.as_str()))
+                .map(|url| serde_json::Value::String(url.to_string()))
+                .collect();
+            job_info["builds"] = serde_json::Value::Array(build_urls);
+        }
+
+        Ok(job_info)
     }
 }
